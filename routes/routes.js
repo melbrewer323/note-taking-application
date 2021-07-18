e7d6da362abbdb31bc5d98549f068549d660bc80
Melissa Brewer <melramirez727@gmail.com>
2021-07-18 00:36:08 -0500

    linked css and js to server/ created api routes/ added new dep id uuid/ added get request to get notes for db.json/ working on post request

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,24 @@
+const { response } = require('express');
 const fs = require('fs');
 const path = require('path');
-// const db = require('./db/db.json')
+let notes = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'db/db.json'), "utf8"));
 
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => 
         res.sendFile(path.join(__dirname, '..', 'db/db.json')));
-    // app.get('/api/notes', (req, res)=> res.json()
+    app.get('/api/notes/:id', (req, res) => {
+        res.json(notes[req.params.id]);
+        res.json(true);
+    });
 
-
-
-
-
-
+    app.post("/api/notes", (req, res) => {
+      let newNote = req.body;
+       notes.push(newNote);
+       console.log(newNote);
+        fs.writeFile('db/db.json', JSON.stringify(notes), (err) => {
+            if (err) throw err;
+        });
+    res.json(newNote);    
+    })
 };
--- server.js
+++ server.js
@@ -6,17 +6,12 @@ const PORT = process.env.PORT || 3001;
 
 require('./routes/routes.js')(app);
 
-
-
-
-
-
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
-
 app.use(express.static(path.join(__dirname, './public')));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
